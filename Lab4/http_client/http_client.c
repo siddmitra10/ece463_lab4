00d6317734693f012594414b0368aad3bbb12b86
Sidd Mitra <67185810+siddmitra10@users.noreply.github.com>
2022-11-26 01:11:04 -0500

    Add files via upload

---
+++ Lab4/http_client/http_client.c
@@ -0,0 +1,29 @@
+/* The code is subject to Purdue University copyright policies.
+ * DO NOT SHARE, DISTRIBUTE, OR POST ONLINE
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4) {
+        fprintf(stderr, "usage: ./http_client [host] [port number] [filepath]\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
